785bab71ef1b5e3b87b5edf41cf7efd349152fb9
Jake Brockway <jakebrockway@microsoft.com>
2024-01-10 17:02:22 -0800

    Blowing up Code
    Rewriting pretty large swathes of code to try and get one shell to start

--- UvLayerOuterUnity/Assets/UvShellCreator.cs
+++ UvLayerOuterUnity/Assets/UvShellCreator.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
 public class UvShellCreator : MonoBehaviour
@@ -20,86 +21,46 @@ public class UvShellCreator : MonoBehaviour
 
     public static List<UvShell> GetUvShells(Mesh mesh)
     {
-        List<UvShell> uvShells = new List<UvShell>();
-        List<UvShell> CompletedUvShells = new List<UvShell>(); //Make a list of completed UV shells that we can borrow away from AllUvShells
         List<UvShell> AllUvShells = new List<UvShell>(); //Define a list that contains the entire shell array
+        List<UvShell> IntermediateUvShells = new List<UvShell>(); //Make a list that holds information for shells being made
+        List<UvShell> CompletedUvShells = new List<UvShell>(); //Make a list of completed UV shells that we can borrow away from AllUvShells
+        
 
         for (int x = 0; x < mesh.triangles.Length; x+= 3) //add all the tris (17,910)
         {
-            UvShell uvShell = new UvShell(mesh.triangles[x], mesh.triangles[x+1], mesh.triangles[x+2]);
-            AllUvShells.Add(uvShell);
+            UvShell Tri = new UvShell(mesh.triangles[x], mesh.triangles[x+1], mesh.triangles[x+2]);
+            AllUvShells.Add(Tri);
         }
-        //Debug.Log($"The size of AllUvShells is {AllUvShells.Count}");
+
+        
 
         //UvShell testUv = AllUvShells[1]; You can call specific UV Tris by calling the index
         UvShell firstShell = AllUvShells[0]; //Add the first tri by indexing from all the shells
+        IntermediateUvShells.Add(firstShell);
+            
 
-        uvShells.Add(firstShell);
+        List<UvShell> shellsConnectedToPoint = GetShellsConnectedToPoint(IntermediateUvShells, mesh).ToList();
 
-        for (int i = 3; i < mesh.triangles.Length; i += 3)
-        {
-            int triPointA = mesh.triangles[i];
-            int triPointB = mesh.triangles[i + 1];
-            int triPointC = mesh.triangles[i + 2];
 
-            List<UvShell> shellsConnectedToPoint = GetShellsConnectedToPoint(AllUvShells, triPointA, triPointB, triPointC).ToList(); //Comes back with an inversed of removed shells from the list
-
-            if(shellsConnectedToPoint.Count == 0) // Create new shell
-            {
-                UvShell newShell = new UvShell(triPointA, triPointB, triPointC);
-                CompletedUvShells.Add(newShell); //Add the finished shell to this list
-                //AllUvShells.Remove(newShell);
-            }
-            else if(shellsConnectedToPoint.Count == 1) // Add it to one shell
-            {
-                shellsConnectedToPoint[0].AddTriangle(triPointA, triPointB, triPointC);
-                //then what does this do?
-            }
-            else // It is connected to multiple shells. Merge the shells and add triangle.
-            {
-                UvShell newShell = GetMergedShell(shellsConnectedToPoint);
-                foreach (UvShell shell1 in shellsConnectedToPoint)
-                {
-                    uvShells.Remove(shell1);
-                }
-                newShell.AddTriangle(triPointA, triPointB, triPointC);
-                uvShells.Add(newShell);
-            }
-        }
-        return uvShells;
+        return AllUvShells;
     }
 
-    private static UvShell GetMergedShell(List<UvShell> shellsConnectedToPoint)
-    {
-        List<int> indicies = new List<int>();
-        foreach (UvShell item in shellsConnectedToPoint)
-        {
-            indicies.AddRange(item.GetIndices());
-        }
-        return new UvShell(indicies.ToArray());
-    }
+    //private static UvShell GetMergedShell(List<UvShell> shellsConnectedToPoint)
+    //{
 
-    private static IEnumerable<UvShell> GetShellsConnectedToPoint(List<UvShell> inputShellList, int triPointA, int triPointB, int triPointC)
+    //}
+
+    private static List<UvShell> GetShellsConnectedToPoint(List<UvShell> inputShellList, Mesh mesh)
     {
-        foreach (UvShell shell in inputShellList)
-        {
-            if (shell.ContainsUv(triPointA)|| shell.ContainsUv(triPointB)|| shell.ContainsUv(triPointC))
-            {
-                inputShellList.Remove(shell);
-                yield return shell; //need to create a system that removes the used shells and only checks against shells in AllShells list
-            }
-        }
+        int triPointA = mesh.triangles[0];
+        int triPointB = mesh.triangles[1];
+        int triPointC = mesh.triangles[2];
     }
 }
 
 public class UvShell
 {
     private readonly HashSet<int> uvs;
-
-    public IEnumerable<int> GetIndices()
-    {
-        return uvs;
-    }
 
     public UvShell(params int[] indicies)
     {
