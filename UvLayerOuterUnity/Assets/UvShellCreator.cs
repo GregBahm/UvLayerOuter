7292aec816c71818fd7779c25784ca31209ba3ec
Jake Brockway <jakebrockway@microsoft.com>
2024-01-12 17:18:11 -0800

    Log Connecting Logic
    Added a function that logs all connecting squares and puts them in a new list while also removing from AllTris. Number is currently 1820 but thats because its not combining these tris, just finding the various connections. Going to make logic for finding connections on connections. Then finally adding all those tris to a UvShell

--- UvLayerOuterUnity/Assets/UvShellCreator.cs
+++ UvLayerOuterUnity/Assets/UvShellCreator.cs
@@ -22,8 +22,9 @@ public class UvShellCreator : MonoBehaviour
     public static List<Tri> GetUvShells(Mesh mesh)
     {
         List<Tri> AllTris = new List<Tri>(); //Define a list that contains the entire shell array
-        List<Tri> IntermediateUvShells = new List<Tri>(); //Make a list that holds information for shells being made
-        List<int> CompletedUvShells = new List<int>(); //Make a list of completed UV shells that we can borrow away from AllUvShells
+        List<Tri> IntermediateShells = new List<Tri>(); //Make a list that holds information for shells being made
+
+        List<UvShell> CompletedUvShells = new List<UvShell>(); //Make a list of completed UV shells that we can borrow away from AllUvShells
 
 
         for (int x = 0; x < mesh.triangles.Length; x += 3) //add all the tris to AllUvShells
@@ -32,13 +33,48 @@ public class UvShellCreator : MonoBehaviour
             AllTris.Add(Tri);
         }
 
+        for (int x = 0; x < AllTris.Count; x++)
+        {
 
+            List <Tri> LoggedTris = LogConnectingTris(AllTris[x], AllTris); // returns a list of connected Tris and removes them from AllTris
 
+            // ################## Add logic for if (LoggedTris.Any()) then chech if those have any connections until it bring back nothing (else) #################
 
+        }
         return AllTris;
     }
+    private static List<Tri> LogConnectingTris(Tri InputTriangle, List<Tri> AllTris) // Log intersecting triangles
+    {
+        
+        List<Tri> LoggedTris = Enumerable.Range(0, AllTris.Count)
+            .Where(i => AllTris[i].GetTri().Intersect(InputTriangle.GetTri()).Any())
+            .Select(i => AllTris[i])
+            .ToList();
 
+        if (LoggedTris.Any())
+        {
+            for (int i = 0; i < LoggedTris.Count; i++)
+            {
+                    AllTris.Remove(LoggedTris[i]); //Remove the LoggedTris from AllTris
+            }
+
+            return LoggedTris;
+
+        }
+        else
+        {
+            LoggedTris.Add(InputTriangle); //The triangle has no connections, so it must be it's own shell. 
+
+            return LoggedTris;
+        }
+
+
+    }
 }
+
+
+
+
 public class Tri
 {
     private readonly List<int> Uvs;
